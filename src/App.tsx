9afa79703f6c85dcff152386901a4f35152abbbb
Markus Wedler <markuswedler8@gmail.com>
2023-05-05 21:57:32 +1000

    Add disk type 2 (no type 1 yet)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ export default () => {
   return (
     <main>
       <div className="container">
-        
+        <input type="checkbox" /> Turn rotation on/off
+        <div className="gramophone">
+          <div className="disk type-2" />
+        </div>
       </div>
     </main>
   )
