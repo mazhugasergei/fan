4f373d6a856995402890061a74fb1f2dd0fa2e5e
Markus Wedler <markuswedler8@gmail.com>
2023-05-07 16:45:33 +1000

    Edit disk 1, add audio control, add progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useRef, useState } from "react"
+
 export default () => {
+  const [isPlaying, setIsPlaying] = useState(false)
+  const audioRef = useRef<HTMLAudioElement>(null)
+  const progressRef = useRef<HTMLDivElement>(null)
+
+  useEffect(()=>{
+    audioRef.current?.addEventListener("timeupdate", (e)=>{
+      if(progressRef.current) progressRef.current.style.width = audioRef.current!.currentTime / audioRef.current!.duration * 100 + "%"
+    })
+  }, [])
+
+  useEffect(()=>{
+    isPlaying ? audioRef.current?.play() : audioRef.current?.pause()
+  }, [isPlaying])
+
   return (
     <main>
       <div className="container">
-        <input type="checkbox" /> Turn rotation on/off
+        <audio ref={audioRef} src="/audio/Heartaches.mp3" />
+        <input type="checkbox" onChange={(e)=>{setIsPlaying(e.target.checked)}} />
         <div className="gramophone">
-          <div className="disk type-1" />
-          <div className="disk type-2" />
-          <audio src=""></audio>
+          <div className={`disk type-1 ${isPlaying ? "is-playing" : ""}`} />
+          {/* <div className={`disk type-2 ${isPlaying ? "is-playing" : ""}`} /> */}
+          <div className="progress"><div ref={progressRef}/></div>
         </div>
       </div>
     </main>
